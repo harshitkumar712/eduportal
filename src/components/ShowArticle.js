d40c5374803801b5ef4800b8f3e11be063b7d492
harshitkumar712 <harshitkumar712@gmail.com>
2020-11-01 17:31:11 +0530

    show article changed

--- src/components/ShowArticle.js
+++ src/components/ShowArticle.js
@@ -1,28 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import ReactHtmlParser from "react-html-parser";
-import axios from 'axios';
-import qs from "qs";
+// import axios from 'axios';
+// import qs from "qs";
 const ShowArticle = (props) => {
-	const [article, setArticle] = useState("");
-	useEffect(() => {
-		const data = {
-			artId:props.match.params.id,
-		}
-   
-		axios({
-			method: "post",
-			url: 'http://192.187.126.18:8082/home/viewArticle',
-			data: qs.stringify(data),
-			// url: "http://192.187.126.18:8082/home/viewArticle",
-		}).then((result) => {
-			console.log(result);
-			setArticle(result.data.Data.body);
-		});
-	}, []);
-
 	
-
-	return <div className=" container">{ReactHtmlParser(article)}</div>;
+	return <div className=" container">{ReactHtmlParser(props.location.body)}</div>;
 };
 
 export default ShowArticle;
--- src/components/ViewSubAdmin.js
+++ src/components/ViewSubAdmin.js
@@ -1,7 +1,77 @@
 import React from "react";
+import Header from "./Header";
+import "../styles/viewsubadmin.css";
 
-const ViewSubAdmin = () => {
-	return <div>ViewSubAdmim</div>;
+const SubAdminData = [
+	{
+		id: 1,
+		Name: "Rakesh",
+		Email: "Email",
+		date: "10 Oct",
+	},
+	{
+		id: 2,
+		Name: "Suresh",
+		Email: "Email",
+		date: "10 Oct",
+	},
+	{
+		id: 3,
+		Name: "Ram",
+		Email: "Email",
+		date: "10 Oct",
+	},
+	{
+		id: 4,
+		Name: "Aryan",
+		Email: "Email",
+		date: "10 Oct",
+	},
+	{
+		id: 5,
+		Name: "Raj",
+		Email: "Email",
+		date: "10 Oct",
+	},
+];
+const ViewSubAdmin = (props) => {
+	const row = SubAdminData.map((item, index) => {
+		return (
+			<React.Fragment key={index}>
+				<tr>
+					<td>{item.id}</td>
+					<td>{item.Name}</td>
+					<td>{item.Email}</td>
+					<td>{item.date}</td>
+				</tr>
+			</React.Fragment>
+		);
+	});
+
+	return (
+		<div>
+			<Header props={props} />
+			<div className="viewsubadmin ">
+				<div className='container-fluid'>
+				<div className="table-item ">
+					<div style={{ overflowX: "auto" }}>
+						<table>
+							<thead>
+								<tr>
+									<th>ID</th>
+									<th>NAME</th>
+									<th>EMAIL</th>
+									<th>DATE</th>
+								</tr>
+							</thead>
+							<tbody>{row}</tbody>
+						</table>
+					</div>
+				</div>
+			</div>
+		</div>
+		</div>
+	);
 };
 
 export default ViewSubAdmin;
